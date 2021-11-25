5d9b2438f2548a234f02420fc57033a982277e65
jesaspiring <jesiahrj@gmail.com>
2021-11-25 13:03:10 +0800

    Modified Instrument Datatable

--- public/dataTables/instruments-datatable.js
+++ public/dataTables/instruments-datatable.js
@@ -1,27 +1,32 @@
 $(function () {
-
     $('#tblInstruments').DataTable({
-        "dom": '<lBf<t>ip>',
+        "dom": '<lBQf<t>ip>',
         buttons: [
+            {
+                text: 'New',
+                action:function(){
+                   $('#modal-new-instrument').modal('show');
+                }
+            },
             {
                 extend: 'copy',
                 exportOptions: {
                     stripHtml: false,
-                    columns: [0, 2, 3, 4, 5, 6, 7]
+                    columns: [1, 2, 3, 4, 5, 6, 7]
                 }
             },
             {
                 extend: 'excel',
                 exportOptions: {
                     stripHtml: false,
-                    columns: [0, 2, 3, 4, 5, 6, 7]
+                    columns: [1, 2, 3, 4, 5, 6, 7]
                 }
             },
             {
                 extend: 'print',
                 exportOptions: {
                     stripHtml: false,
-                    columns: [0, 2, 3, 4, 5, 6, 7]
+                    columns: [1, 2, 3, 4, 5, 6, 7]
                 }
             }
         ],
@@ -30,11 +35,11 @@ $(function () {
         "processing": true,
         "serverside": true,
         "responsive": true,
-        "ajax": "src/DataTables/InstrumentTable.php",
+        "ajax": "src/DataTables/InstrumentsTable.php",
         columnDefs: [
             {
-                targets: [1],
-                visible: false
+                targets: [0, 8, 9],
+                visible: false,
             }
         ]
     })
