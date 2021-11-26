a415074ffde44535e109c7c68e7109eea9144248
jesaspiring <jesiahrj@gmail.com>
2021-11-26 16:24:14 +0800

    Removed parameter updating

--- src/Controller/UpdateInstrument.php
+++ src/Controller/UpdateInstrument.php
@@ -2,13 +2,9 @@
 
 include $_SERVER['DOCUMENT_ROOT'] . 'InstruDB/src/Classes/Database.php';
 include $_SERVER['DOCUMENT_ROOT'] . 'InstruDB/src/Classes/Instruments.php';
-include $_SERVER['DOCUMENT_ROOT'] . 'InstruDB/src/Classes/Parameters.php';
 
 $database = new Database();
 $instrument = new Instruments($database->DatabaseConnection());
-$instrumentParameter = new Parameters($database->DatabaseConnection());
-
-$saveSuccess = false;
 
 if ($_POST) {
 
@@ -22,29 +18,5 @@ if ($_POST) {
 
     $updateInstrument = $instrument->UpdateInstrument($_POST['instrumentId']);
 
-    $parameters = json_decode($_POST['parameters'], true);
-
-    if (COUNT($parameters) > 0) {
-
-        foreach ($parameters as $parameter) {
-            if (!isset($parameter['parameterId'])) {
-                $instrumentParameter->parameterName = $parameter['parameterName'];
-                $instrumentParameter->parameterValue = $parameter['parameterValue'];
-                $instrumentParameter->dateCalibration = date("Y-m-d", strtotime($parameter['dateCalibration']));
-
-                $saveSuccess = $instrumentParameter->AddInstrumentParameter($_POST['instrumentId']);
-            } else {
-                $instrumentParameter->parameterName = $parameter['parameterName'];
-                $instrumentParameter->parameterValue = $parameter['parameterValue'];
-                $instrumentParameter->dateCalibration = date("Y-m-d", strtotime($parameter['dateCalibration']));
-
-                $saveSuccess = $instrumentParameter->UpdateExistingParameter($parameter['parameterId']);
-            }
-        }
-    } else {
-        $saveSuccess = $updateInstrument;
-    }
-
-
-    echo json_encode($saveSuccess);
+    echo json_encode($updateInstrument);
 }
