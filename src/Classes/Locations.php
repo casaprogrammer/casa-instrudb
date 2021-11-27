17c1e9446d9c437d41d0d482c7b4e194a1e1654f
jesaspiring <jesiahrj@gmail.com>
2021-11-27 14:11:49 +0800

    Added functions for add and updating

--- src/Classes/Locations.php
+++ src/Classes/Locations.php
@@ -5,11 +5,35 @@ class Locations
     private $tableName = "locations";
     private $con;
 
+    public $locationName;
     public $locations = array();
 
     public function __construct($database)
     {
         $this->con = $database;
+    }
+
+    public function AddLocation()
+    {
+        $query = "INSERT INTO " . $this->tableName . "
+                  SET location_name=:locationName";
+
+        $insertStatement = $this->con->prepare($query);
+        $insertStatement->bindParam(':locationName', $this->locationName);
+
+        return ($insertStatement->execute()) ?? false;
+    }
+
+    public function UpdateLocationDetail($id)
+    {
+        $query = "UPDATE " . $this->tableName . "
+                  SET location_name=:locationName
+                  WHERE id = $id";
+
+        $updateStatement = $this->con->prepare($query);
+        $updateStatement->bindParam(':locationName', $this->locationName);
+
+        return ($updateStatement->execute()) ?? false;
     }
 
     public function GetLocations()
@@ -18,7 +42,7 @@ class Locations
         $selectStatement = $this->con->prepare($query);
         $selectStatement->execute();
 
-        while($row = $selectStatement->fetch(PDO::FETCH_ASSOC)){
+        while ($row = $selectStatement->fetch(PDO::FETCH_ASSOC)) {
             $locations[] = $row;
         }
 
