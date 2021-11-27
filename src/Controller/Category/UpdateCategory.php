2ca37168420b4d55e08763eced56641d771b06cc
jesaspiring <jesiahrj@gmail.com>
2021-11-27 13:29:03 +0800

    Initial Commit

---
+++ src/Controller/Category/UpdateCategory.php
@@ -0,0 +1,15 @@
+<?php
+
+include $_SERVER['DOCUMENT_ROOT'] . 'InstruDB/src/Classes/Database.php';
+include $_SERVER['DOCUMENT_ROOT'] . 'InstruDB/src/Classes/Categories.php';
+
+$database = new Database();
+$category = new Categories($database->DatabaseConnection());
+
+
+if ($_POST) {
+
+    $category->categoryName = $_POST['categoryName'];
+
+    echo json_encode($category->UpdateCategoryDetail($_POST['id']));
+}
