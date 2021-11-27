743cd92836312e3dbcf646600df651163996ad88
jesaspiring <jesiahrj@gmail.com>
2021-11-27 13:39:13 +0800

    Refactored Code

--- src/Controller/Category/GetAllCategory.php
+++ src/Controller/Category/GetAllCategory.php
@@ -6,16 +6,5 @@ include $_SERVER['DOCUMENT_ROOT'] . 'InstruDB/src/Classes/Categories.php';
 $database = new Database();
 $categories = new Categories($database->DatabaseConnection());
 
-$details = array();
 
-if ($_GET) {
-
-    foreach($categories as $category){
-        $data['categoryId'] = $row['id'];
-        $data['categoryName'] = $row['category_name'];
-
-        array_push($details, $data);
-    }
-
-    echo json_encode($details);
-}
+echo json_encode($categories->GetAllCategories());
