baa086ef6730accba3087b9149002240f460e720
jesaspiring <jesiahrj@gmail.com>
2021-12-08 13:55:40 +0800

    Added function for editing log

--- src/Classes/Logbook.php
+++ src/Classes/Logbook.php
@@ -32,4 +32,22 @@ class Logbook
 
         return ($insertStatement->execute()) ?? false;
     }
+
+    public function EditLog($id)
+    {
+        $query = "UPDATE " . $this->tableName . "
+                  SET
+                  details=:details, log_type=:logType, 
+                  log_date=:logDate
+                  WHERE id=:id";
+
+        $updateStatement = $this->con->prepare($query);
+
+        $updateStatement->bindParam(":details", $this->details);
+        $updateStatement->bindParam(":logType", $this->logType);
+        $updateStatement->bindParam(":logDate", $this->logDate);
+        $updateStatement->bindParam(":id", $id);
+
+        return ($updateStatement->execute()) ?? false;
+    }
 }
